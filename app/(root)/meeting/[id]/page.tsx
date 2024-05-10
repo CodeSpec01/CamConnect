a37337cbc6ef3bcb4fa2da668d3c5977fdae7e59
CodeSpec <online.codespec@gmail.com>
2024-05-11 01:02:40 +0530

    Added custom hook to fetch call details

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+'use client';
+
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useState } from 'react';
 
 const Meeting = ({ params }: { params: { id: string } }) => {
+
+    const {user, isLoaded} = useUser();
+    const [isSetupComplete, setIsSetupComplete] = useState(false);
+    
     return (
-        <div className='text-white'>
-            Meeting Room #{params.id}
-        </div>
+        
+        <main className='h-screen w-full'>
+            
+            <StreamCall>
+                
+                <StreamTheme>
+
+                    {!isSetupComplete ? (
+                        <MeetingSetup />
+                    ) : (
+                        <MeetingRoom />
+                    )}
+                </StreamTheme>
+            </StreamCall>
+        </main>
     )
 }
 
