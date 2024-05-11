71771c4b1bde85c3e2d3dc318817e83ad5b18a27
CodeSpec <online.codespec@gmail.com>
2024-05-11 22:59:38 +0530

    added useGetCalls hook

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,10 +1,16 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Upcoming = () => {
   return (
-    <div>
-      Upcoming
-    </div>
+    <section className=' flex size-full flex-col gap-10 '>
+
+      <h1 className=' text-3xl font-bold '>
+        Upcomming Meeting
+      </h1>
+
+      <CallList type='upcoming' />
+    </section>
   )
 }
 
---
+++ components/CallList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
+    return (
+        <div>
+            Call List
+        </div>
+    )
+}
+
+export default CallList
