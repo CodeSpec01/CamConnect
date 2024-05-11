34140942c015bc428ced9c46214b0498e09c3414
CodeSpec <online.codespec@gmail.com>
2024-05-12 00:21:31 +0530

    Previous meeting, recordings complete

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,10 +1,18 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Recordings = () => {
+
+  
   return (
-    <div>
-      Recordings
-    </div>
+    <section className=' flex size-full flex-col gap-10 '>
+
+      <h1 className=' text-3xl font-bold '>
+        Recordings
+      </h1>
+
+      <CallList type='recordings' />
+    </section>
   )
 }
 
--- components/CallList.tsx
+++ components/CallList.tsx
@@ -4,15 +4,17 @@ import Upcoming from '@/app/(root)/(home)/upcoming/page';
 import { useGetCalls } from '@/hooks/useGetCalls'
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MeetingCard from './MeetingCard';
 import Loader from './Loader';
+import { useToast } from './ui/use-toast';
 
 const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
 
     const { endedCalls, isLoading, callRecordings, upcomingCalls } = useGetCalls();
     const router = useRouter();
     const [recordings, setRecordings] = useState<CallRecording[]>([])
+    const { toast } = useToast();
 
     const getCalls = () => {
 
@@ -50,18 +52,41 @@ const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
         }
     }
 
+    useEffect(() => {
+
+        const fetchRecordings = async () => {
+
+            try {
+
+                const callData = await Promise.all(callRecordings.map((meeting) => meeting.queryRecordings()));
+
+                const recordings = callData
+                    .filter(call => call.recordings.length > 0)
+                    .flatMap(call => call.recordings)
+
+                setRecordings(recordings);
+            } catch (error) {
+
+                toast({title: 'Please try again later'})
+            }
+        }
+
+        if (type === 'recordings') fetchRecordings();
+    }, [type, callRecordings])
+
     const calls = getCalls();
     const noCallsMessage = getNoCallsMessage();
 
-    if(isLoading) return <Loader />
+    if (isLoading) return <Loader />
 
     return (
 
-        <div className='grid grid-cols-1 gap-5 lg:grid-cols-5'>
+        <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3'>
 
             {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording) => (
                 <MeetingCard
                     key={(meeting as Call).id}
+
                     icon={
                         type === 'ended'
                             ? '/icons/previous.svg'
@@ -69,13 +94,20 @@ const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
                                 ? '/icons/upcoming.svg'
                                 : '/icons/recordings.svg'
                     }
-                    title={(meeting as Call).state.custom.description.substring(0,20) || 'No Description'}
-                    date={(meeting as Call).state.startsAt?.toLocaleString()!}
+
+                    title={(meeting as Call).state?.custom.description.substring(0, 20) || (meeting as CallRecording).filename.substring(0, 20) || 'No Description'}
+
+                    date={(meeting as Call).state?.startsAt?.toLocaleString()!}
+
                     isPreviousMeeting={type === 'ended'}
+
                     buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
+
                     buttonText={type === 'recordings' ? 'Play' : 'Start'}
-                    handleClick={type === 'recordings' ? () => router.push(`${meeting.url}`) : () => router.push(`/meeting/${(meeting as Call).id}`)}
-                    link={type === 'recordings' ? meeting.url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`}
+
+                    handleClick={type === 'recordings' ? () => router.push(`${(meeting as CallRecording).url}`) : () => router.push(`/meeting/${(meeting as Call).id}`)}
+
+                    link={type === 'recordings' ? (meeting as CallRecording).url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`}
                 />
             )) : (
                 <h1>{noCallsMessage}</h1>
