d6084c6ea3d64fe399a313848f865579e17c3b98
CodeSpec <online.codespec@gmail.com>
2024-05-11 12:47:00 +0530

    meeting setup complete

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { Call, VideoPreview, useCall } from '@stream-io/video-react-sdk'
+import { Call, DeviceSettings, VideoPreview, useCall } from '@stream-io/video-react-sdk'
 import React, { useEffect, useState } from 'react'
+import { Button } from './ui/button';
 
-const MeetingSetup = () => {
-  
+const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComplete : (value: boolean) => void}) => {
+
   const call = useCall();
-  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);  
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
   const [timer, setTimer] = useState(false);
 
   setTimeout(() => {
@@ -15,24 +16,47 @@ const MeetingSetup = () => {
 
   useEffect(() => {
 
-    if(isMicCamToggledOn) {
-      
+    if (isMicCamToggledOn) {
+
       call?.camera.disable();
       call?.microphone.disable();
-      
+
     } else {
 
       call?.camera.enable();
       call?.microphone.enable();
     }
   }, [isMicCamToggledOn, call?.camera, call?.microphone])
-  
+
   return (
     <div className='flex h-screen w-full flex-col items-center justify-center gap-3'>
 
       <h1 className='text-2xl font-bold'>Setup</h1>
 
-      { timer &&<VideoPreview />}
+      {timer && <VideoPreview />}
+
+      <div className='flex h-16 items-center justify-center gap-3'>
+
+        <label className='flex items-center justify-center gap-2 font-medium'>
+
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn} onChange={(e) =>
+              setIsMicCamToggledOn(e.target.checked)}
+          />
+
+          Join with mic and camera off
+        </label>
+
+        <DeviceSettings />
+      </div>
+
+      <Button className='rounded-md bg-green-500 px-4 py-2.5' onClick={() =>{
+        call?.join();
+        setIsSetupComplete(true);
+      }}>
+        Join Meeting
+      </Button>
     </div>
   )
 }
